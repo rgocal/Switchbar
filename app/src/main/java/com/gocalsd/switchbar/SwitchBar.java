24d4e8e1baf50cd445bf108f1feac3e58d825e57
rgocal <rgocal09@gmail.com>
2018-11-25 23:38:44 -0500

    Initial Push
    Version 1.0 release!

---
+++ app/src/main/java/com/gocalsd/switchbar/SwitchBar.java
@@ -0,0 +1,255 @@
+package com.gocalsd.switchbar;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Google's Switchbar layout widget modified for customization and personal use
+ * Modified by Ryan Gocal
+ * Fin
+ */
+
+public class SwitchBar extends LinearLayout implements CompoundButton.OnCheckedChangeListener,
+        View.OnClickListener {
+
+    private ToggleSwitch mSwitch;
+    private TextView mTextView;
+    private String switchOn, switchOff;
+    private int mSwitchColor;
+    private Drawable switchbarBackground, wrappedBackground, wrappedBackgroundTwo;
+
+    private ArrayList<OnSwitchChangeListener> mSwitchChangeListeners =
+            new ArrayList<OnSwitchChangeListener>();
+
+    public static interface OnSwitchChangeListener {
+        /**
+         * Called when the checked state of the Switch has changed.
+         *
+         * @param switchView The Switch view whose state has changed.
+         * @param isChecked  The new checked state of switchView.
+         */
+        void onSwitchChanged(Switch switchView, boolean isChecked);
+    }
+    public SwitchBar(Context context) {
+        this(context, null);
+    }
+
+    public SwitchBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SwitchBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public void setSwitchbarOnBackground(int backgroundColor){
+        this.mSwitchColor = ContextCompat.getColor(getContext(), backgroundColor);
+
+        switchbarBackground = ContextCompat.getDrawable(getContext(), R.drawable.switchbar_bkg);
+        assert switchbarBackground != null;
+        wrappedBackground = DrawableCompat.wrap(switchbarBackground);
+        DrawableCompat.setTint(wrappedBackground, mSwitchColor);
+        wrappedBackground.invalidateSelf();
+        wrappedBackground.mutate();
+    }
+
+    public void setSwitchbarOffBackground(int backgroundColor){
+        this.mSwitchColor = ContextCompat.getColor(getContext(), backgroundColor);
+
+        switchbarBackground = ContextCompat.getDrawable(getContext(), R.drawable.switchbar_bkg);
+        assert switchbarBackground != null;
+        wrappedBackgroundTwo = DrawableCompat.wrap(switchbarBackground);
+        DrawableCompat.setTint(wrappedBackgroundTwo, mSwitchColor);
+        wrappedBackgroundTwo.invalidateSelf();
+        wrappedBackgroundTwo.mutate();
+    }
+
+    public SwitchBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        LayoutInflater.from(context).inflate(R.layout.switchbar_layout, this);
+        canAnimate();
+        mTextView = findViewById(R.id.switch_text);
+        mTextView.setText(R.string.switch_off_text);
+
+        setBackground(switchbarBackground);
+
+        switchOff = String.valueOf(R.string.switch_off_text);
+        switchOn = String.valueOf(R.string.switch_on_text);
+
+        mSwitch = (ToggleSwitch) findViewById(R.id.switch_widget);
+        // Prevent onSaveInstanceState() to be called as we are managing the state of the Switch
+        // on our own
+        mSwitch.setSaveEnabled(false);
+        addOnSwitchChangeListener(new OnSwitchChangeListener() {
+            @Override
+            public void onSwitchChanged(Switch switchView, boolean isChecked) {
+                setTextViewLabel(isChecked);
+            }
+        });
+        setOnClickListener(this);
+        // Default is hide
+        setVisibility(View.GONE);
+    }
+
+    public void setOnMessage (String onMessage){
+        this.switchOn = String.valueOf(onMessage);
+    }
+
+    public void setOffMessage (String offMessage){
+        this.switchOff = String.valueOf(offMessage);
+    }
+
+    //Update the text and background by state
+    public void setTextViewLabel(boolean isChecked) {
+        mTextView.setText(isChecked ? switchOn : switchOff);
+        setBackground(isChecked ? wrappedBackground : wrappedBackgroundTwo);
+    }
+
+    public void setChecked(boolean checked) {
+        setTextViewLabel(checked);
+        mSwitch.setChecked(checked);
+    }
+
+    public void setCheckedInternal(boolean checked) {
+        setTextViewLabel(checked);
+        mSwitch.setCheckedInternal(checked);
+    }
+
+    public boolean isChecked() {
+        return mSwitch.isChecked();
+    }
+
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        mTextView.setEnabled(enabled);
+        mSwitch.setEnabled(false);
+    }
+
+    public final ToggleSwitch getSwitch() {
+        return mSwitch;
+    }
+
+    public void show() {
+        if (!isShowing()) {
+            setVisibility(View.VISIBLE);
+            mSwitch.setOnCheckedChangeListener(this);
+        }
+    }
+
+    public void hide() {
+        if (isShowing()) {
+            setVisibility(View.GONE);
+            mSwitch.setOnCheckedChangeListener(null);
+        }
+    }
+
+    public boolean isShowing() {
+        return (getVisibility() == View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final boolean isChecked = !mSwitch.isChecked();
+        setChecked(isChecked);
+    }
+
+    public void propagateChecked(boolean isChecked) {
+        final int count = mSwitchChangeListeners.size();
+        for (int n = 0; n < count; n++) {
+            mSwitchChangeListeners.get(n).onSwitchChanged(mSwitch, isChecked);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        propagateChecked(isChecked);
+    }
+
+    public void addOnSwitchChangeListener(OnSwitchChangeListener listener) {
+        if (mSwitchChangeListeners.contains(listener)) {
+            throw new IllegalStateException("Cannot add twice the same OnSwitchChangeListener");
+        }
+        mSwitchChangeListeners.add(listener);
+    }
+
+    public void removeOnSwitchChangeListener(OnSwitchChangeListener listener) {
+        if (!mSwitchChangeListeners.contains(listener)) {
+            throw new IllegalStateException("Cannot remove OnSwitchChangeListener");
+        }
+        mSwitchChangeListeners.remove(listener);
+    }
+
+    static class SavedState extends BaseSavedState {
+        boolean checked;
+        boolean visible;
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+        /**
+         * Constructor called from {@link #CREATOR}
+         */
+        private SavedState(Parcel in) {
+            super(in);
+            checked = (Boolean)in.readValue(null);
+            visible = (Boolean)in.readValue(null);
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeValue(checked);
+            out.writeValue(visible);
+        }
+
+        @Override
+        public String toString() {
+            return "SwitchBar.SavedState{"
+                    + Integer.toHexString(System.identityHashCode(this))
+                    + " checked=" + checked
+                    + " visible=" + visible + "}";
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState ss = new SavedState(superState);
+        ss.checked = mSwitch.isChecked();
+        ss.visible = isShowing();
+        return ss;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        SavedState ss = (SavedState) state;
+        super.onRestoreInstanceState(ss.getSuperState());
+        mSwitch.setCheckedInternal(ss.checked);
+        setTextViewLabel(ss.checked);
+        setVisibility(ss.visible ? View.VISIBLE : View.GONE);
+        mSwitch.setOnCheckedChangeListener(ss.visible ? this : null);
+        requestLayout();
+    }
+}
---
+++ switchbarExample/src/main/java/com/gocalsd/switchbarexample/MainActivity.java
@@ -0,0 +1,71 @@
+package com.gocalsd.switchbarexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.gocalsd.switchbar.SwitchBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    SwitchBar switchBar;
+    String posMessage, negMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        posMessage = "Switchbar is Enabled!";
+        negMessage = "Switchbar Disabled...";
+
+        switchBar = findViewById(R.id.switchBar);
+        //Show the Switchbar! It's hidden by default!
+        switchBar.show();
+
+        //Set the on and off Messages
+        switchBar.setOnMessage(posMessage);
+        switchBar.setOffMessage(negMessage);
+        //Set the on and off Background Colors
+        //It's suggested to use the Primary and DarkPrimary colors of your application
+        //as the switch uses the Accent Color
+        switchBar.setSwitchbarOnBackground(R.color.colorPrimary);
+        switchBar.setSwitchbarOffBackground(R.color.colorPrimaryDark);
+
+        //Set the defaults
+        switchBar.setChecked(true);
+        switchBar.setTextViewLabel(true);
+
+        //Set the addOnSwitchChangeListener
+        switchBar.addOnSwitchChangeListener(new SwitchBar.OnSwitchChangeListener() {
+            @Override
+            public void onSwitchChanged(Switch switchView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(MainActivity.this, "It's on!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "It's off!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
